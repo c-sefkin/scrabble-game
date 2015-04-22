2cc9c4c30e56a6fbea0234704b32d6e996e1890d
Chris Sefkin <c.sefkin@gmail.com>
2015-04-22 11:35:27 -0700

    updated app.rb

--- views/app.rb
+++ views/app.rb
@@ -1,6 +1,13 @@
 require('sinatra')
 require('sintatra/reloader')
+require('scrabble_score')
+also_reload('lib/**/*.rb')
 
 get('/') do
   erb(:index)
+end
+
+get('/title') do
+  @title = params.fetch('title').scrabble_score()
+  erb(:title)
 end
