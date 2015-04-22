3fb156ec3c3f9096643239e5be2da6e5a2b45197
Chris Sefkin <c.sefkin@gmail.com>
2015-04-22 11:10:02 -0700

    final project files added

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -1,8 +1,17 @@
-require ('rspec')
-require ('scrabble_score')
+require('rspec')
+require('scrabble_score')
 
-describe ('String#scrabble_score') do
-  it ("") do
-    expect
+# return a scrabble score for a letter
+# returns a scrabble score for a word
+
+
+
+describe('String#scrabble_score') do
+  it("returns a scrabble score for a letter") do
+    expect(("a").scrabble_score()).to(eq(1))
+  end
+
+  it ("returns a scrabble score for a word") do
+    expect(("grow").scrabble_score()).to(eq(8))
   end
 end
